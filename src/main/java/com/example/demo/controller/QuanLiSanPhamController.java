c25358aef662cba693a4f8d6bd37a16b45367e9e
sao1111 <saonvph45620@fpt.fptedu.vn>
2025-03-03 00:03:34 +0700

    sao222

--- src/main/java/com/example/demo/controller/QuanLiSanPhamController.java
+++ src/main/java/com/example/demo/controller/QuanLiSanPhamController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -92,7 +93,76 @@ public class QuanLiSanPhamController {
                     .body("Lỗi: " + e.getMessage());
         }
     }
+//    danhMuc===============================================================
+    @GetMapping("chat-lieu")
+    public String chatlieu(Model model) {
+        List<ChatLieu> ds = this.chatLieuRepository.findAll();
+        model.addAttribute("listct", ds);
+        return "san_pham/chatlieu";
+    }
 
+    @GetMapping("/danh-sach-chat-lieu/{id}")
+    @ResponseBody
+    public ResponseEntity<ChatLieu> dsmasaaa(@PathVariable("id") Integer idct) {
+        Optional<ChatLieu> result = chatLieuRepository.findByIdCt(idct);
+        return result.map(mauSac -> new ResponseEntity<>(mauSac, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/them-chat-lieu")
+    public ResponseEntity<?> themScM(@RequestBody Map<String, Object> payload) {
+        ChatLieu chatLieu = new ChatLieu();
+        chatLieu.setTenChatLieu((String) payload.get("tenChatLieu"));
+        chatLieu.setMoTa((String) payload.get("moTa"));
+        chatLieuRepository.save(chatLieu);
+        return ResponseEntity.ok("Chất liệu đã được thêm thành công!");
+    }
+
+    @PutMapping("/cap-nhat-chat-lieu/{id}")
+    public ResponseEntity<?> updateSaM(@PathVariable Integer id, @RequestBody Map<String, Object> payload) {
+
+        ChatLieu chatLieu = chatLieuRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Chất liệu không tồn tại"));
+        chatLieu.setTenChatLieu((String) payload.get("tenChatLieu"));
+        chatLieu.setMoTa((String) payload.get("moTa"));
+        chatLieuRepository.save(chatLieu);
+        return ResponseEntity.ok("Chất liệu đã được cập nhật thành công!");
+    }
+
+    @GetMapping("danh-muc")
+    public String listddh(Model model) {
+        List<DanhMuc> ds = this.danhMucRepository.findAll();
+        model.addAttribute("listdm", ds);
+        return "san_pham/danhmuc";
+    }
+
+    @GetMapping("/danh-sach-danh-muc/{id}")
+    @ResponseBody
+    public ResponseEntity<DanhMuc> dsmasasss(@PathVariable("id") Integer iddm) {
+        Optional<DanhMuc> result = danhMucRepository.findByIdDm(iddm);
+        return result.map(danhMuc -> new ResponseEntity<>(danhMuc, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/them-danh-muc")
+    public ResponseEntity<?> themdm(@RequestBody Map<String, Object> payload) {
+        DanhMuc danhMuc = new DanhMuc();
+        danhMuc.setTendanhmuc((String) payload.get("tenDanhMuc"));
+        danhMuc.setMota((String) payload.get("moTa"));
+        danhMucRepository.save(danhMuc);
+        return ResponseEntity.ok("Danh muc đã được thêm thành công!");
+    }
+
+    @PutMapping("/cap-nhat-danh-muc/{id}")
+    public ResponseEntity<?> updateDM(@PathVariable Integer id, @RequestBody Map<String, Object> payload) {
+
+        DanhMuc danhMuc = danhMucRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Danh muc không tồn tại"));
+        danhMuc.setTendanhmuc((String) payload.get("tenDanhMuc"));
+        danhMuc.setMota((String) payload.get("moTa"));
+        danhMucRepository.save(danhMuc);
+        return ResponseEntity.ok("Danh muc đã được thêm thành công!");
+    }
 
 
 }
--- src/main/java/com/example/demo/repository/DanhMucRepository.java
+++ src/main/java/com/example/demo/repository/DanhMucRepository.java
@@ -2,8 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DanhMuc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DanhMucRepository extends JpaRepository<DanhMuc,Integer> {
+    Optional<DanhMuc> findByTendanhmuc(String tendanhmuc);
+
+    @Query("SELECT h FROM DanhMuc h WHERE h.id = :iddm")
+    Optional<DanhMuc> findByIdDm(@Param("iddm") Integer iddm);
 }
